a2488e0bc2252f7a0a4911bde55dcc3d79278f13
Matt Ericksen <ericksenm93@ymail.com>
2020-07-28 00:46:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 def load_library(path)
   # code goes here
-  em = YAML.load_file('lib/emoticons.yml')
+  emoticons = YAML.load_file(path)
   final = {}
   em.each do |k,v|
     final[k] = Hash.new
