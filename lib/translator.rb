6d82f7532e8e4267e24d8f44263e30b75eb60892
Matt Ericksen <ericksenm93@ymail.com>
2020-07-28 00:20:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,6 @@ def load_library(emotes)
   em = YAML.load_file('lib/emoticons.yml')
   final = {}
   em.each do |k,v|
-    final[k][:english] = v[0]
-    final[k][:japanese] = v[1]
     puts final
   end
   final
