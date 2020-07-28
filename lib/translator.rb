80e30ed78f56a7e7aace9d107d84070b8f363842
Matt Ericksen <ericksenm93@ymail.com>
2020-07-28 00:19:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
+def load_library(emotes)
   # code goes here
+  em = YAML.load_file('lib/emoticons.yml')
+  final = {}
+  em.each do |k,v|
+    final[k][:english] = v[0]
+    final[k][:japanese] = v[1]
+    puts final
+  end
+  final
 end
 
 def get_japanese_emoticon
